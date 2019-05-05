1a16568d52a2d518d7cd3d41b81696c394ba2264
Sumner Fields <sumnertfields@gmail.com>
2019-05-05 02:16:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+var beatlesPlay = []
+for (var i = 0; i < 4; i++) {
+beatlesPlay.push(musicians[i] + " plays " + instruments[i])}
+return beatlesPlay}
+
+function johnLennonFacts (facts) {
+  var johnLennon = []
+  for (var i = 0; i === facts.length; i++) {
+    johnLennon.push(facts[i]+"!!!")
+  }
+  return johnLennon
+}
